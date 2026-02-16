70e3c65a5a0cf718e23bcd6493793069c343e16d
kali113 <kali113@users.noreply.github.com>
2026-02-16 20:25:29 +0100

    feat: add GitHub Pages support to SvelteKit build
    - Add conditional adapter selection (static vs Cloudflare)
    - Set base path for GitHub Pages deployment
    - Add GITHUB_PAGES env variable to deploy workflow

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,15 +1,25 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import cloudflare from '@sveltejs/adapter-cloudflare';
+import adapterStatic from '@sveltejs/adapter-static';
+
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: isGitHubPages
+			? adapterStatic({
+					fallback: 'index.html'
+				})
+			: cloudflare({
+					// See below for an explanation of these options
+					routes: {
+						include: ['/*'],
+						exclude: ['<all>']
+					}
+				}),
+		paths: {
+			base: isGitHubPages ? '/Anglicus' : ''
+		}
 	}
 };
 
